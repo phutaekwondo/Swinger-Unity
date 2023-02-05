aa539e44a40e3ec909dd58ea8c422651d9a8f659
phutaekwondo <phutaekwondo2001@gmail.com>
2023-02-05 22:40:49 +0700

    implement MaintainDistance method

--- Assets/Scripts/HardJoint2D.cs
+++ Assets/Scripts/HardJoint2D.cs
@@ -7,6 +7,13 @@ public class HardJoint2D : MonoBehaviour
     //REFERENCES
     [SerializeField] GameObject m_connectedGameObject;
 
+    //PUBLIC METHODS
+
+    //PRIVATE METHODS
+    private void Start() {
+        m_distance = Vector2.Distance(transform.position, m_connectedGameObject.transform.position);
+    }
+
     private void Update() 
     {
         MaintainDistance();
@@ -22,5 +29,11 @@ public class HardJoint2D : MonoBehaviour
 
     private void MaintainDistance()
     {
+        Vector2 direction = m_connectedGameObject.transform.position - transform.position;
+        float currentDistance = direction.magnitude;
+        direction.Normalize();
+
+        Vector2 newPostion = (Vector2)transform.position + direction * (currentDistance - m_distance);
+        transform.position = newPostion;
     }
 }
