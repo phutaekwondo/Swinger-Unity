4868bc6d0ec758f9214ae230709cf4eb74c27834
phutaekwondo <phutaekwondo2001@gmail.com>
2023-02-06 23:25:51 +0700

    implement HardJoint2D

--- Assets/Scripts/HardJoint2D.cs
+++ Assets/Scripts/HardJoint2D.cs
@@ -6,25 +6,53 @@ public class HardJoint2D : MonoBehaviour
     private float m_distance;
     //REFERENCES
     [SerializeField] GameObject m_connectedGameObject;
+    Rigidbody2D m_rigidbody;
 
     //PUBLIC METHODS
 
     //PRIVATE METHODS
     private void Start() {
         m_distance = Vector2.Distance(transform.position, m_connectedGameObject.transform.position);
+        m_rigidbody = GetComponent<Rigidbody2D>();
     }
 
     private void Update() 
     {
         MaintainDistance();
         UpdateVeclocity();
-
     }
 
     private void UpdateVeclocity()
     {
-        //apply gravity
         //rotate the vector
+        Vector2 connectDirection = (Vector2)(m_connectedGameObject.transform.position - this.transform.position);
+        connectDirection.Normalize();
+        Vector2 velocity = m_rigidbody.velocity;
+        float angle = Vector2.Angle(velocity, connectDirection);
+
+        //cos(a-90) = Sin(a)
+        float velocityWeight = velocity.magnitude;
+        float newVelocityWeight = velocityWeight * Mathf.Sin((angle/180) * Mathf.PI);
+
+        Vector2 newVelocityDirection_NegativeY = new Vector2( -connectDirection.y, connectDirection.x);
+        Vector2 newVelocityDirection_NegativeX = new Vector2( connectDirection.y, -connectDirection.x);
+
+        Vector2 newVelocityDirection;
+        if (Vector2.Angle(newVelocityDirection_NegativeX, velocity) > Vector2.Angle(newVelocityDirection_NegativeY,velocity))
+        {
+            newVelocityDirection = newVelocityDirection_NegativeY;
+        }
+        else
+        {
+            newVelocityDirection = newVelocityDirection_NegativeX;
+        }
+
+        Vector2 newVelocity = newVelocityDirection * newVelocityWeight;
+
+        //apply gravity
+        //Unity physics already apply gravity, so take a good rest.
+
+        m_rigidbody.velocity = newVelocity;
     }
 
     private void MaintainDistance()
